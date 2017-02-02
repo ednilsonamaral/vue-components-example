1c462f0aed705ff64a388a73bff75814f64dee9e
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2017-02-02 00:16:37 -0200

    finalizando componentes

--- src/components/contato/index.vue
+++ src/components/contato/index.vue
@@ -1,5 +1,49 @@
 <template>
-  <h1>Página de Contato</h1>
+  <div>
+    <p>Trabalhar com Vue.js é muito gostoso, demais! E outra, é fácil!</p>
+
+    <p>Estou amando cada vez mais esse lindezo!</p>
+
+    <p>Está precisando de algum projeto com Vue.js? Não sabe qual dev contratar? Pois bem, encontrou, EUZINHO AQUI! Tem alguma dúvida quando ao Vue.js? Precisa de ajuda em algum projeto? Entre em contato comigo para batermos um papo legal e resolvemos juntos!</p>
+
+    <div class="col-lg-5" style="margin: 50px 0;">
+      <div class="list-group">
+        <a href="http://ednilsonamaral.me" target="_blank" class="list-group-item">
+          <h4 class="list-group-item-heading">Portfolio</h4>
+          <p class="list-group-item-text">http://ednilsonamaral.me</p>
+        </a>
+
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading">E-mail</h4>
+          <p class="list-group-item-text">ednilsonamaral.ti@gmail.com</p>
+        </a>
+
+        <a href="https://www.facebook.com/ednilson.amaral" class="list-group-item" target="_blank">
+          <h4 class="list-group-item-heading">Facebook</h4>
+          <p class="list-group-item-text">Ednilson Amaral</p>
+        </a>
+
+        <a href="https://twitter.com/ednilsonaamaral" target="_blank" class="list-group-item">
+          <h4 class="list-group-item-heading">Twitter</h4>
+          <p class="list-group-item-text">@ednilsonaamaral</p>
+        </a>
+
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading">Skype</h4>
+          <p class="list-group-item-text">ednilson_amaral</p>
+        </a>
+
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading">Telegram</h4>
+          <p class="list-group-item-text">@ednilsonamaral</p>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-12">
+      <p>É isso aí, folks! Até mais! Abraços! :)</p>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/components/sobre/index.vue
+++ src/components/sobre/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <h1>Página de Sobre</h1>
+  <div>
+    <p>Nesse projeto utilizei as seguintes tecnologias para desenvolvimento:</p>
+
+    <div class="list-group">
+      <a href="#" class="list-group-item">
+        <span class="badge">2.1</span>
+        Vue.js
+      </a>
+
+      <a href="#" class="list-group-item">
+        <span class="badge">2.2</span>
+        vue-router
+      </a>
+
+      <a href="#" class="list-group-item">vue-cli > webpack</a>
+
+      <a href="#" class="list-group-item">
+        <span class="badge">3</span>
+        Bootstrap
+      </a>
+
+      <a href="#" class="list-group-item">Sass</a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +31,7 @@ export default {
 </script>
 
 <style scoped>
+.badge{
+  background-color: #41b883;
+}
 </style>
